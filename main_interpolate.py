a9e2ac86ccbc3ed77b194a25eeed8f85429d7630
sunnymac <spnthsen@gmail.com>
2022-09-01 23:08:02 +0530

    Modified the program for single band images also.

--- main_interpolate.py
+++ main_interpolate.py
@@ -1,8 +1,9 @@
 """
 Created on 2022, 23 Aug (23/08/22) at 20:10
-    Title: main_interpolate.py - Interpolating the input multi-band image of some size by a given factor
+    Title: main_interpolate.py - Interpolating the input single/multi-band image of some size by a given factor.
     Description:
-        -   ...
+        -   Interpolating the given image by a factor.
+        -   Using different interpolation methods to interpolate.
 @author: Supantha Sen, sunnymac, IISc Bangalore
 """
 
@@ -20,11 +21,14 @@ img = np.array( Image.open('./Image_Samples/lena_color.tiff') )
 factor = int( input('Enter the factor by which you want to interpolate: ') )
 inter_type = input('Enter the interpolation type (''linear'', ''cubic'', ''quintic''): ')
 
-num_band = img.shape[2]
-img_interpol = np.zeros((factor*img.shape[0], factor*img.shape[1], img.shape[2]), dtype=img.dtype)
-
-for b in range(num_band):
-    img_interpol[:,:,b] = img_interpolate(img[:,:,b], factor, inter_type)
+if len(img.shape) == 2:
+    img_interpol = np.zeros((factor * img.shape[0], factor * img.shape[1]), dtype=img.dtype)
+    img_interpol = img_interpolate(img, factor, inter_type)
+else:
+    num_band = img.shape[2]
+    img_interpol = np.zeros((factor*img.shape[0], factor*img.shape[1], img.shape[2]), dtype=img.dtype)
+    for b in range(num_band):
+        img_interpol[:,:,b] = img_interpolate(img[:,:,b], factor, inter_type)
 
 
 print('Input Image Size =', img.shape[0:2])
