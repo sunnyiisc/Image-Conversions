1ba60636b9d8d3ddeffbb645cd1363a06c2bce7d
sunnymac <spnthsen@gmail.com>
2022-09-01 22:55:08 +0530

    inputing the type of interpolation from the program.

--- main_interpolate.py
+++ main_interpolate.py
@@ -18,12 +18,13 @@ from image_interpolation import *
 img = np.array( Image.open('./Image_Samples/lena_color.tiff') )
 
 factor = int( input('Enter the factor by which you want to interpolate: ') )
+inter_type = input('Enter the interpolation type (''linear'', ''cubic'', ''quintic''): ')
 
 num_band = img.shape[2]
 img_interpol = np.zeros((factor*img.shape[0], factor*img.shape[1], img.shape[2]), dtype=img.dtype)
 
 for b in range(num_band):
-    img_interpol[:,:,b] = img_interpolate(img[:,:,b], factor)
+    img_interpol[:,:,b] = img_interpolate(img[:,:,b], factor, inter_type)
 
 
 print('Input Image Size =', img.shape[0:2])
